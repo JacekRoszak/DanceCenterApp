a17e308299391ae9f18f5dbe75958718bc2a0fc9
JacekRoszak <65806090+JacekRoszak@users.noreply.github.com>
2020-08-12 01:02:00 +0200

    pictures

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params, admin: :true)
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to @user
